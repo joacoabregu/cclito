3fe8e47e8a84f61c71d4f0ae7831af02c2a7821a
Joaquin Abregu <abregujoaquin@gmail.com>
2023-08-13 20:02:49 -0300

    feat: add StockPriceSchema

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,3 +14,15 @@ export const StocksSchema = StockSchema.array();
 export type Stock = z.infer<typeof StockSchema>;
 export type Stocks = z.infer<typeof StocksSchema>;
 export type StockName = Pick<Stock, 'full_name' | 'description'>;
+
+export const StockPriceSchema = z.object({
+  t: z.number().array().nonempty(),
+  o: z.number().array(),
+  h: z.number().array(),
+  l: z.number().array(),
+  c: z.number().array().nonempty(),
+  v: z.number().array(),
+  s: z.string(),
+});
+
+export type StockPrice = z.infer<typeof StockPriceSchema>;
