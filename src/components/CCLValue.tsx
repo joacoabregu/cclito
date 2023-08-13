44a7f831ccfebde3ae11d997688168a878e500df
Joaquin Abregu <abregujoaquin@gmail.com>
2023-08-13 15:25:20 -0300

    refactor: Autocomplete state

--- src/components/CCLValue.tsx
+++ src/components/CCLValue.tsx
@@ -5,63 +5,12 @@ import useSWR from 'swr';
 import { fetcher } from '@utils/index';
 import type { Stocks } from '@customTypes/index';
 
-type AutcompleteProps = {
-  items: Stocks;
-  value: string;
-  onChange: (val: string) => void;
-};
+type AutcompleteProps = {};
 
 //we are using dropdown, input and menu component from daisyui
 const Autocomplete = memo(function Autocomplete(props: AutcompleteProps) {
-  const { items, value, onChange } = props;
   const ref = useRef<HTMLDivElement>(null);
   const [open, setOpen] = useState(false);
-  return (
-    <div
-      // use classnames here to easily toggle dropdown open
-      className={classNames({
-        'dropdown w-full': true,
-        'dropdown-open': open,
-      })}
-      ref={ref}
-    >
-      <input
-        type='text'
-        className='input input-bordered w-full'
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        placeholder='Type something..'
-        tabIndex={0}
-      />
-      <div className='dropdown-content bg-base-200 top-14 max-h-96 overflow-auto flex-col rounded-md'>
-        <ul
-          className='menu menu-compact '
-          // use ref to calculate the width of parent
-          style={{ width: ref.current?.clientWidth }}
-        >
-          {items.map((item, index) => {
-            return (
-              <li
-                key={index}
-                tabIndex={index + 1}
-                onClick={() => {
-                  onChange(item.full_name);
-                  setOpen(false);
-                }}
-                className='border-b border-b-base-content/10 w-full'
-              >
-                <button>
-                  {item.full_name} <span> ({item.description})</span>{' '}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
-  );
-});
-export default function Value() {
   const [searchTerm, setSearchTerm] = useState('');
   const debouncedSearchTerm = useDebounce(searchTerm, 400);
   //Debounce useSwr
@@ -71,18 +20,63 @@ export default function Value() {
       debouncedSearchTerm ? `api/stock?name=${debouncedSearchTerm}` : null,
     fetcher
   );
+  const items = data?.stocks || [];
+  return (
+    <>
+      {error && <p>Se ha producido un error. Intente más tarde.</p>}
 
+      {!error && (
+        <div
+          // use classnames here to easily toggle dropdown open
+          className={classNames({
+            'dropdown w-full': true,
+            'dropdown-open': open,
+          })}
+          ref={ref}
+        >
+          <input
+            type='text'
+            className='input input-bordered w-full'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder='Ingresar el nombre de un CEDEAR'
+            tabIndex={0}
+          />
+          <div className='dropdown-content bg-base-200 top-14 max-h-96 overflow-auto flex-col rounded-md'>
+            <ul
+              className='menu menu-compact '
+              // use ref to calculate the width of parent
+              style={{ width: ref.current?.clientWidth }}
+            >
+              {items.map((item, index) => {
+                return (
+                  <li
+                    key={index}
+                    tabIndex={index + 1}
+                    onClick={() => {
+                      setSearchTerm(item.full_name);
+                      setOpen(false);
+                    }}
+                    className='border-b border-b-base-content/10 w-full'
+                  >
+                    <button>
+                      {item.full_name} <span> ({item.description})</span>{' '}
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
+  );
+});
+export default function Value() {
   return (
     <div className='container mx-auto px-4 flex justify-center'>
       <div className='py-10 max-w-md w-full'>
-        {error && <p>Se ha producido un error. Intente más tarde.</p>}
-        {!error && (
-          <Autocomplete
-            value={searchTerm}
-            onChange={setSearchTerm}
-            items={data?.stocks || []}
-          />
-        )}
+        <Autocomplete />
       </div>
     </div>
   );
