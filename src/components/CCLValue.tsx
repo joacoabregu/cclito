f463021450bbddc8d47a07d545a24faa9ec506a9
Joaquin Abregu <abregujoaquin@gmail.com>
2023-08-13 16:08:29 -0300

    feat: add spinner, handle empty results, stock state

--- src/components/CCLValue.tsx
+++ src/components/CCLValue.tsx
@@ -4,11 +4,15 @@ import { useDebounce } from '@uidotdev/usehooks';
 import useSWR from 'swr';
 import { fetcher } from '@utils/index';
 import type { Stocks } from '@customTypes/index';
+import Spinner from './Spinner';
 
-type AutcompleteProps = {};
+type AutcompleteProps = {
+  setStockName: React.Dispatch<React.SetStateAction<string>>;
+};
 
 //we are using dropdown, input and menu component from daisyui
 const Autocomplete = memo(function Autocomplete(props: AutcompleteProps) {
+  const { setStockName } = props;
   const ref = useRef<HTMLDivElement>(null);
   const [open, setOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
@@ -20,63 +24,80 @@ const Autocomplete = memo(function Autocomplete(props: AutcompleteProps) {
       debouncedSearchTerm ? `api/stock?name=${debouncedSearchTerm}` : null,
     fetcher
   );
-  const items = data?.stocks || [];
+
   return (
     <>
       {error && <p>Se ha producido un error. Intente más tarde.</p>}
-
       {!error && (
-        <div
-          // use classnames here to easily toggle dropdown open
-          className={classNames({
-            'dropdown w-full': true,
-            'dropdown-open': open,
-          })}
-          ref={ref}
-        >
-          <input
-            type='text'
-            className='input input-bordered w-full'
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder='Ingresar el nombre de un CEDEAR'
-            tabIndex={0}
-          />
-          <div className='dropdown-content bg-base-200 top-14 max-h-96 overflow-auto flex-col rounded-md'>
-            <ul
-              className='menu menu-compact '
-              // use ref to calculate the width of parent
-              style={{ width: ref.current?.clientWidth }}
-            >
-              {items.map((item, index) => {
-                return (
+        <>
+          <div
+            // use classnames here to easily toggle dropdown open
+            className={classNames({
+              'dropdown w-full': true,
+              'dropdown-open': open,
+            })}
+            ref={ref}
+          >
+            <input
+              type='text'
+              className='input input-bordered w-full'
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              placeholder='Ingresar el nombre de un CEDEAR'
+              tabIndex={0}
+            />
+            {isLoading && <Spinner css='absolute top-4 right-3' />}
+            <div className='dropdown-content bg-base-200 top-14 max-h-96 overflow-auto flex-col rounded-md'>
+              <ul
+                className='menu menu-compact '
+                // use ref to calculate the width of parent
+                style={{ width: ref.current?.clientWidth }}
+              >
+                {data?.stocks &&
+                  data?.stocks?.length > 0 &&
+                  data?.stocks.map((item, index) => {
+                    return (
+                      <li
+                        key={index}
+                        tabIndex={index + 1}
+                        onClick={() => {
+                          setStockName(item.full_name);
+                          setOpen(false);
+                        }}
+                        className='border-b border-b-base-content/10 w-full'
+                      >
+                        <button>
+                          {item.full_name} <span> ({item.description})</span>{' '}
+                        </button>
+                      </li>
+                    );
+                  })}
+                {data?.stocks && data?.stocks.length === 0 && (
                   <li
-                    key={index}
-                    tabIndex={index + 1}
+                    tabIndex={1}
                     onClick={() => {
-                      setSearchTerm(item.full_name);
                       setOpen(false);
                     }}
                     className='border-b border-b-base-content/10 w-full'
                   >
-                    <button>
-                      {item.full_name} <span> ({item.description})</span>{' '}
-                    </button>
+                    <button>No se han encontrado resultados.</button>
                   </li>
-                );
-              })}
-            </ul>
+                )}
+              </ul>
+            </div>
           </div>
-        </div>
+        </>
       )}
     </>
   );
 });
 export default function Value() {
+  const [stockName, setStockName] = useState('');
+
   return (
     <div className='container mx-auto px-4 flex justify-center'>
       <div className='py-10 max-w-md w-full'>
-        <Autocomplete />
+        <Autocomplete setStockName={setStockName} />
       </div>
     </div>
   );
