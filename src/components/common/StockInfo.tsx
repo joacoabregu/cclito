63b10194f4843cb67abd0c4eb72c6a8e7c842e0c
Joaquin Abregu <abregujoaquin@gmail.com>
2023-08-19 20:56:25 -0300

    feat: add Fav

--- src/components/common/StockInfo.tsx
+++ src/components/common/StockInfo.tsx
@@ -1,6 +1,7 @@
 import Spinner from '@components/Spinner';
 import type { Ratio, StockName, StockPrice } from '@customTypes/index';
 import { fetcher, getStockDetails } from '@utils/index';
+import useLocalStorage from '@utils/useStorage';
 import useSWR from 'swr';
 
 export interface StockInfoDetailsProps {
@@ -16,7 +17,10 @@ export function StockInfo({
   detailsComponent,
 }: {
   stockName: StockName | undefined;
-  detailsComponent: (details: StockInfoDetailsProps) => JSX.Element;
+  detailsComponent: (
+    details: StockInfoDetailsProps,
+    stockName: StockName | undefined
+  ) => JSX.Element;
 }) {
   //Debounce useSwr
   //https://gist.github.com/csandman/cb1b9cae2334415b0b20e04b228c1016
@@ -28,7 +32,6 @@ export function StockInfo({
     () => (stockName ? `api/stock/price?name=${stockName.full_name}` : null),
     fetcher
   );
-
   const details = getStockDetails(data);
 
   return (
@@ -36,7 +39,7 @@ export function StockInfo({
       {stockName && <StockName stockName={stockName} />}
       {error && <CedearError />}
       {isLoading && <Spinner />}
-      {data && detailsComponent(details)}
+      {data && detailsComponent(details, stockName)}
     </>
   );
 }
@@ -60,28 +63,86 @@ export function CedearError() {
 
 export function CCLInfoDetails({
   details,
+  stockName,
 }: {
   details: StockInfoDetailsProps;
+  stockName: StockName | undefined;
 }) {
+  const [favs, setFavs] = useLocalStorage<StockName[]>('favorites', []);
+  const isFav = favs?.some((fav) => fav.full_name === stockName?.full_name);
+  const saveStock = () => {
+    if (isFav) {
+      setFavs(favs.filter((fav) => fav.full_name !== stockName?.full_name));
+    } else {
+      setFavs([...favs, ...(stockName ? [stockName] : [])]);
+    }
+  };
   return (
-    <div>
-      <p className='text-2xl'>CCL: ${details.CCL} </p>
-      <p className='text-1xl'>Ratio: {details.ratio} </p>
-      <p className='text-base'>Precio: usd$ {details.stockPrice}</p>
-      <p className='text-base'>Precio CEDEAR: ${details.cedearPrice}</p>
-      <p>Valor de cierre del: {details.date}</p>
-    </div>
+    <>
+      <div>
+        <p className='text-2xl'>CCL: ${details.CCL} </p>
+        <p className='text-1xl'>Ratio: {details.ratio} </p>
+        <p className='text-base'>Precio: usd$ {details.stockPrice}</p>
+        <p className='text-base'>Precio CEDEAR: ${details.cedearPrice}</p>
+        <p>Valor de cierre del: {details.date}</p>
+      </div>
+      <div>
+        {!isFav && (
+          <img
+            className='hover:scale-125 cursor-pointer'
+            onClick={saveStock}
+            src='data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIGQ9Ik0xMiAyMS41OTNjLTUuNjMtNS41MzktMTEtMTAuMjk3LTExLTE0LjQwMiAwLTMuNzkxIDMuMDY4LTUuMTkxIDUuMjgxLTUuMTkxIDEuMzEyIDAgNC4xNTEuNTAxIDUuNzE5IDQuNDU3IDEuNTktMy45NjggNC40NjQtNC40NDcgNS43MjYtNC40NDcgMi41NCAwIDUuMjc0IDEuNjIxIDUuMjc0IDUuMTgxIDAgNC4wNjktNS4xMzYgOC42MjUtMTEgMTQuNDAybTUuNzI2LTIwLjU4M2MtMi4yMDMgMC00LjQ0NiAxLjA0Mi01LjcyNiAzLjIzOC0xLjI4NS0yLjIwNi0zLjUyMi0zLjI0OC01LjcxOS0zLjI0OC0zLjE4MyAwLTYuMjgxIDIuMTg3LTYuMjgxIDYuMTkxIDAgNC42NjEgNS41NzEgOS40MjkgMTIgMTUuODA5IDYuNDMtNi4zOCAxMi0xMS4xNDggMTItMTUuODA5IDAtNC4wMTEtMy4wOTUtNi4xODEtNi4yNzQtNi4xODEiLz48L3N2Zz4='
+          />
+        )}
+        {isFav && (
+          <img
+            className='hover:scale-125 cursor-pointer'
+            onClick={saveStock}
+            src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTIgNC40MTljLTIuODI2LTUuNjk1LTExLjk5OS00LjA2NC0xMS45OTkgMy4yNyAwIDcuMjcgOS45MDMgMTAuOTM4IDExLjk5OSAxNS4zMTEgMi4wOTYtNC4zNzMgMTItOC4wNDEgMTItMTUuMzExIDAtNy4zMjctOS4xNy04Ljk3Mi0xMi0zLjI3eiIvPjwvc3ZnPg=='
+          ></img>
+        )}
+      </div>
+    </>
   );
 }
 
-export function CedearInfoDetails({ details }: { details: StockInfoDetailsProps }) {
+export function CedearInfoDetails({
+  details,
+  stockName,
+}: {
+  details: StockInfoDetailsProps;
+  stockName: StockName | undefined;
+}) {
+  const [favs, setFavs] = useLocalStorage<StockName[]>('favorites', []);
+  const saveStock = () => {
+    const isFav = favs?.some((fav) => fav.full_name === stockName?.full_name);
+    if (isFav) {
+      setFavs(favs.filter((fav) => fav.full_name === stockName?.full_name));
+    } else {
+      setFavs([...favs, ...(stockName ? [stockName] : [])]);
+    }
+  };
+  const isFav = favs?.some((fav) => fav.full_name === stockName?.full_name);
   return (
-    <div>
-      <p className='text-1xl'>Precio CEDEAR: ${details.cedearPrice}</p>
-      <p className='text-base'>Precio: usd$ {details.stockPrice}</p>
-      <p className='text-base'>CCL: ${details.CCL} </p>
-      <p className='text-base'>Ratio: {details.ratio} </p>
-      <p>Valor de cierre del: {details.date}</p>
-    </div>
+    <>
+      <div>
+        <p className='text-1xl'>Precio CEDEAR: ${details.cedearPrice}</p>
+        <p className='text-base'>Precio: usd$ {details.stockPrice}</p>
+        <p className='text-base'>CCL: ${details.CCL} </p>
+        <p className='text-base'>Ratio: {details.ratio} </p>
+        <p>Valor de cierre del: {details.date}</p>
+      </div>
+      <div>
+        {!isFav && (
+          <img
+            onClick={saveStock}
+            src='data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIGQ9Ik0xMiAyMS41OTNjLTUuNjMtNS41MzktMTEtMTAuMjk3LTExLTE0LjQwMiAwLTMuNzkxIDMuMDY4LTUuMTkxIDUuMjgxLTUuMTkxIDEuMzEyIDAgNC4xNTEuNTAxIDUuNzE5IDQuNDU3IDEuNTktMy45NjggNC40NjQtNC40NDcgNS43MjYtNC40NDcgMi41NCAwIDUuMjc0IDEuNjIxIDUuMjc0IDUuMTgxIDAgNC4wNjktNS4xMzYgOC42MjUtMTEgMTQuNDAybTUuNzI2LTIwLjU4M2MtMi4yMDMgMC00LjQ0NiAxLjA0Mi01LjcyNiAzLjIzOC0xLjI4NS0yLjIwNi0zLjUyMi0zLjI0OC01LjcxOS0zLjI0OC0zLjE4MyAwLTYuMjgxIDIuMTg3LTYuMjgxIDYuMTkxIDAgNC42NjEgNS41NzEgOS40MjkgMTIgMTUuODA5IDYuNDMtNi4zOCAxMi0xMS4xNDggMTItMTUuODA5IDAtNC4wMTEtMy4wOTUtNi4xODEtNi4yNzQtNi4xODEiLz48L3N2Zz4='
+          />
+        )}
+        {isFav && (
+          <img src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTIgNC40MTljLTIuODI2LTUuNjk1LTExLjk5OS00LjA2NC0xMS45OTkgMy4yNyAwIDcuMjcgOS45MDMgMTAuOTM4IDExLjk5OSAxNS4zMTEgMi4wOTYtNC4zNzMgMTItOC4wNDEgMTItMTUuMzExIDAtNy4zMjctOS4xNy04Ljk3Mi0xMi0zLjI3eiIvPjwvc3ZnPg=='></img>
+        )}
+      </div>
+    </>
   );
 }
