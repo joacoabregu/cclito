8abbf591beba9483dc2dd063e11b93d249cbc3ee
Joaquin Abregu <abregujoaquin@gmail.com>
2023-08-22 17:33:14 -0300

    feat: add Alert for errors

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -13,6 +13,7 @@ export default function Favorites() {
   }>(() => (favsQuery ? `api/stocks?name=${favsQuery}` : null), fetcher);
   return (
     <div className='container mx-auto max-w-4xl mx-auto px-4'>
+      {error && <Alert />}
       {isDesktop && (
         <div className='overflow-x-auto'>
           <table className='table table-zebra w-full'>
@@ -136,5 +137,32 @@ function FavoritesTableRow({
         />
       </td>
     </tr>
+  );
+}
+
+function Alert() {
+  return (
+    <div
+      id='toast-danger'
+      className='flex items-center w-full z-10 max-w-sm p-4 mb-4 text-gray-400 bg-gray-800 rounded-lg shadow fixed top-5 right-5 '
+      role='alert'
+    >
+      <div className='inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200'>
+        <svg
+          className='w-5 h-5'
+          aria-hidden='true'
+          xmlns='http://www.w3.org/2000/svg'
+          fill='currentColor'
+          viewBox='0 0 20 20'
+        >
+          <path d='M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z' />
+        </svg>
+        <span className='sr-only'>Error icon</span>
+      </div>
+      <div className='ml-3 text-sm font-normal'>
+        Se ha producido un error. La información de las acciones puede ser
+        errónea.
+      </div>
+    </div>
   );
 }
