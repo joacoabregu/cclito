1a867dbaf605336634474c87c07d5565101bb45c
Joaquin Abregu <abregujoaquin@gmail.com>
2023-08-22 19:51:39 -0300

    feat: add cedear info props

--- src/components/Stock.tsx
+++ src/components/Stock.tsx
@@ -17,7 +17,10 @@ export default function Stock({
     <div className='container mx-auto max-w-3xl px-4 h-screen grid place-items-center text-center'>
       <div className='flex flex-col items-center'>
         <p className='text-3xl mb-4'>{ticker}</p>
-        <CedearInfoDetails details={details} />
+        <CedearInfoDetails
+          details={details}
+          stockName={{ full_name: ticker, description: '' }}
+        />
       </div>
     </div>
   );
