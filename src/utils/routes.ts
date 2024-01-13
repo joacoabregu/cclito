5a01d4ec35860610fd1faa88de9544c985d23a6e
Joaquin Abregu <abregujoaquin@gmail.com>
2024-01-13 20:04:25 -0300

    feat: declare paths routes as const for better type definition

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,12 +1,12 @@
 export const PATHS = {
 	HOME: '/',
 	FAVORITES: '/favoritos',
-	api: '/api'
-};
+	api: 'api'
+} as const;
 
 const API_STOCK = `${PATHS.api}/stock`;
 
 export const PATHS_API = {
 	stock: { url: API_STOCK, price: `${API_STOCK}/price}` },
 	stocks: `${PATHS.api}/stocks`
-};
+} as const;
