ef6020dd74b3e6b4e907b6ee37f1117c38d18b75
Joaquin Abregu <abregujoaquin@gmail.com>
2024-01-20 17:03:06 -0300

    fix: route typo error

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -7,6 +7,6 @@ export const PATHS = {
 const API_STOCK = `${PATHS.api}/stock`;
 
 export const PATHS_API = {
-	stock: { url: API_STOCK, price: `${API_STOCK}/price}` },
+	stock: { url: API_STOCK, price: `${API_STOCK}/price` },
 	stocks: `${PATHS.api}/stocks`
 } as const;
